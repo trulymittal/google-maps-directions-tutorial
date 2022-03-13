16bf9cf6229883896e5d5082f39bc21629352c1c
Truly Mittal <trulymittal@gmail.com>
2022-03-13 18:14:30 +0530

    init

--- src/App.js
+++ src/App.js
@@ -4,15 +4,9 @@ import {
   ButtonGroup,
   chakra,
   Flex,
-  IconButton,
+  HStack,
   Input,
-  Stat,
-  StatArrow,
-  StatHelpText,
-  StatLabel,
-  StatNumber,
-  StatUpArrow,
-  VStack,
+  Text,
 } from '@chakra-ui/react'
 
 function App() {
@@ -20,7 +14,7 @@ function App() {
     <Flex
       position='relative'
       flexDirection='column'
-      bgColor='blue.200'
+      alignItems='center'
       bgImage='https://images.unsplash.com/photo-1647117181799-0ac3e50a548a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
       bgPos='bottom'
       h='100vh'
@@ -28,14 +22,14 @@ function App() {
     >
       <Box position='absolute' left={0} top={0} h='100%' w='100%'></Box>
       <chakra.form
-        maxW='sm'
         p={4}
         borderRadius='lg'
-        m={4}
+        mt={4}
         bgColor='white'
         shadow='base'
+        zIndex='modal'
       >
-        <VStack spacing={4}>
+        <HStack spacing={4}>
           <Input type='text' placeholder='Origin' />
           <Input type='text' placeholder='Destination' />
           <ButtonGroup>
@@ -44,11 +38,11 @@ function App() {
             </Button>
             <Button>Clear</Button>
           </ButtonGroup>
-        </VStack>
-        <Stat mt={4}>
-          <StatLabel>Distance:</StatLabel>
-          <StatLabel>Duration:</StatLabel>
-        </Stat>
+        </HStack>
+        <HStack mt={4} justifyContent='space-evenly'>
+          <Text>Distance:</Text>
+          <Text>Duration:</Text>
+        </HStack>
       </chakra.form>
     </Flex>
   )
