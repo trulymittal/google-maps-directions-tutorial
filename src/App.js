a800060f55a346913a6e734ccbfc21bcf7d8b09b
Truly Mittal <trulymittal@gmail.com>
2022-03-13 21:57:25 +0530

    one

--- src/App.js
+++ src/App.js
@@ -6,24 +6,42 @@ import {
   HStack,
   IconButton,
   Input,
+  SkeletonText,
   Text,
 } from '@chakra-ui/react'
 import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 
+import { useJsApiLoader, GoogleMap } from '@react-google-maps/api'
+
+const center = { lat: 48.8584, lng: 2.2945 }
+
 function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  })
+
+  if (!isLoaded) {
+    return <SkeletonText />
+  }
+
   return (
     <Flex
       position='relative'
       flexDirection='column'
       alignItems='center'
-      bgColor='blue.200'
-      bgImage='https://images.unsplash.com/photo-1647117181799-0ac3e50a548a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
-      bgPos='center'
-      bgSize='cover'
       h='100vh'
       w='100vw'
     >
-      <Box position='absolute' left={0} top={0} h='100%' w='100%'></Box>
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
+        {/* Google Map Box */}
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+        >
+          {/* Displayin markers, or directions */}
+        </GoogleMap>
+      </Box>
       <Box
         p={4}
         borderRadius='lg'
